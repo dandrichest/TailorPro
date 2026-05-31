1a7f08ed800e5df5673fd4e7375fa7a5c47f362b
princegodwyll15 <godwyllprince@gmail.com>
2026-05-31 03:47:27 +0000

    week 4 ui added

--- TailorPro/obj/Debug/net10.0/TailorPro.AssemblyInfo.cs
+++ TailorPro/obj/Debug/net10.0/TailorPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TailorPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30bd09dd16ca0f87ccd8bb1bfd27de449472f9e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993d2817ad8acd82a0b7c5fc8adf91bcaec04f68")]
 [assembly: System.Reflection.AssemblyProductAttribute("TailorPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TailorPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
