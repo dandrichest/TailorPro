8dfa1ff13a5aa67144d6748c9aa6462b91855d9e
DANIEL <dandrichest@gmail.com>
2026-06-06 16:29:50 +0100

    update

--- obj/Debug/net10.0/TailorPro.AssemblyInfo.cs
+++ obj/Debug/net10.0/TailorPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TailorPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d298c92f4133cec1dc91a82bec714a9f2f628f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b74367a20e07afe7ffcf428c0e5dde448ffefb9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TailorPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TailorPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
